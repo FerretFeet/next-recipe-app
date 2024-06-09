8d1e822aad237c5b42dbdb2ddf198708490e8c1a
= <=>
2024-06-09 14:38:51 -0500

    dynamic form inputs for instructions, ingredients, tags. can add/remove element on the end of respective arr

--- src/components/composite/CreateRecipeForm/CreateRecipeForm.tsx
+++ src/components/composite/CreateRecipeForm/CreateRecipeForm.tsx
@@ -4,15 +4,95 @@ import { useState } from "react";
 import styles from "./CreateRecipeForm.module.css";
 
 export default function CreateRecipeForm() {
-  const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({
+    // name: null,
+    // username: null,
+    img: null,
+    description: "",
+    instructions: [""],
+    // prepTime: null,
+    // cookTime: null,
+    // servings: null,
+    ingredients: [{ name: "", quantity: 0, unit: "" }],
+    tags: [{ name: "" }],
+  });
 
   // @ts-expect-error
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setInputs((prevState) => ({ ...prevState, [name]: value }));
-    console.log(inputs);
+    console.log({ ...inputs });
   };
+  const handleAddInstruction = () => {
+    setInputs((prevState) => ({
+      ...prevState,
+      instructions: [...inputs.instructions, ""],
+    }));
+  };
+  const handleRemoveInstruction = () => {
+    if (inputs.instructions.length <= 1) return;
+    setInputs((prevState) => ({
+      ...prevState,
+      instructions: inputs.instructions.slice(0, -1),
+    }));
+  };
+  // @ts-expect-error
+  const handleInputChange = (index, e) => {
+    // @ts-expect-error
+    const newInstructions = [...inputs[e.target.name]];
+    console.log(`handleINPUT debug ${newInstructions}`);
+    newInstructions[index] = { name: e.target.value };
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: newInstructions,
+    }));
+    // @ts-expect-error
+    console.log(inputs[e.target.name]);
+  };
+  // @ts-expect-error
+  const handleIngredientInputChange = (index, e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const newIngredients = inputs.ingredients.map((ingredient, i) => {
+      if (i === index) {
+        return { ...ingredient, [name]: value };
+      }
+      return ingredient;
+    });
+    setInputs((prevState) => ({
+      ...prevState,
+      ingredients: newIngredients,
+    }));
+    console.log(inputs.ingredients);
+  };
+  const handleAddIngredient = () => {
+    setInputs((prevState) => ({
+      ...prevState,
+      ingredients: [...inputs.ingredients, { name: "", quantity: 0, unit: "" }],
+    }));
+  };
+  const handleRemoveIngredient = () => {
+    if (inputs.ingredients.length <= 1) return;
+    setInputs((prevState) => ({
+      ...prevState,
+      ingredients: inputs.ingredients.slice(0, -1),
+    }));
+  };
+  const handleAddTag = () => {
+    setInputs((prevState) => ({
+      ...prevState,
+      tags: [...inputs.tags, { name: "" }],
+    }));
+  };
+  const handleRemoveTag = () => {
+    if (inputs.tags.length <= 1) return;
+    setInputs((prevState) => ({
+      ...prevState,
+      tags: inputs.tags.slice(0, -1),
+    }));
+  };
+
   // @ts-expect-error
   const validateImg = (e) => {
     if (e.target.name.size > 50000000) {
@@ -23,7 +103,8 @@ export default function CreateRecipeForm() {
     // tinypng
     return true;
   };
-  const validateText = (e) => {
+  //@ts-expect-error
+  const validateTextArea = (e) => {
     if (!e.target.value.match(/^[a-zA-Z&\-\(\)',; ]*$/)) {
       return false;
     }
@@ -33,6 +114,9 @@ export default function CreateRecipeForm() {
   //@ts-expect-error
   const handleSubmit = (e) => {
     e.preventDefault();
+    // turn instructions from array into delimited string ';'
+    // turn ingredients into obj arr', ; , , ;'
+    // and tags obj arr
     alert({ ...inputs });
   };
   return (
@@ -95,24 +179,40 @@ export default function CreateRecipeForm() {
             // What should this maxLength be
             maxLength={300}
             required
-            onChange={validateText}
+            onChange={validateTextArea}
           />
         </div>
-        <div className="">
-          {/* DO LIKE TAGS, Duplicate rows then concat on submit */}
-          <label htmlFor="instructions">Instructions: </label>
-          <textarea
-            id="instructions"
-            rows={3}
-            cols={24}
-            name="instructions"
-            // What should this maxLength be
-            maxLength={300}
-            required
-            onChange={validateText}
-          />
-        </div>
-
+        {inputs.instructions.map((instruction, index) => (
+          <div
+            className=""
+            key={index}
+          >
+            {/* DO LIKE TAGS, Duplicate rows then concat on submit */}
+            <label htmlFor="instructions">Instructions: </label>
+            <input
+              id="instructions"
+              type="text"
+              name={`instruction-${index}`}
+              // What should this maxLength be
+              maxLength={120}
+              required
+              pattern="^[0-9a-zA-Z&\-\(\)', ]*$"
+              onChange={(e) => handleInputChange(index, e)}
+            />
+          </div>
+        ))}
+        <button
+          type="button"
+          onClick={handleAddInstruction}
+        >
+          Add Step
+        </button>
+        <button
+          type="button"
+          onClick={handleRemoveInstruction}
+        >
+          Remove Step
+        </button>
         <div className="">
           <label htmlFor="prep-time">Prep Time: </label>
           <input
@@ -148,27 +248,106 @@ export default function CreateRecipeForm() {
         </div>
         <div className="">
           {/* HOW TO GROUP< TAGS FIRST */}
-          <label htmlFor="ingredients">Ingredients: </label>
-          <input
+          <label htmlFor="">Ingredients: </label>
+          {inputs.ingredients.map((ingredient, index) => (
+            <div
+              className=""
+              key={index}
+            >
+              <div className="">
+                <label htmlFor={`ingredient-name-${index}`}>Name:</label>
+                <input
+                  type="text"
+                  id={`ingredient-name-${index}`}
+                  // name={`ingr-name-${1}`}
+                  name="name"
+                  pattern="^[a-zA-Z\-', ]*$"
+                  required
+                  onChange={(e) => handleIngredientInputChange(index, e)}
+                />
+              </div>
+              <div className="">
+                <label htmlFor={`ingredient-quantity-${index}`}>
+                  Quantity:
+                </label>
+                <input
+                  type="number"
+                  id={`ingredient-quantity-${index}`}
+                  // name={`ingr-quantity-${1}`}
+                  name="quantity"
+                  max={99}
+                  min={1}
+                  required
+                  onChange={(e) => handleIngredientInputChange(index, e)}
+                />
+              </div>
+              <div className="">
+                <label htmlFor={`ingredient-unit-${index}`}>Unit:</label>
+                <input
+                  type="text"
+                  id={`ingredient-unit-${index}`}
+                  // name={`ingr-unit-${1}`}
+                  name="unit"
+                  required
+                  pattern="^[a-zA-Z\-', ]*$"
+                  onChange={(e) => handleIngredientInputChange(index, e)}
+                />
+              </div>
+            </div>
+          ))}
+          <button
+            type="button"
+            onClick={handleAddIngredient}
+          >
+            Add Ingredient
+          </button>
+          <button
+            type="button"
+            onClick={handleRemoveIngredient}
+          >
+            Remove Ingredient
+          </button>
+
+          {/* <input
             id="ingredients"
             type="text"
-            name="ingredient"
+            name="ingredients"
             required
-            onChange={validateText}
-          />
+            pattern="^[a-zA-Z&\-\(\)', ]*$"
+          /> */}
+          {/* datalist for measurement unit pull from db */}
+
           {/* datalist for measurement unit pull from db */}
         </div>
-        <div className="">
-          <label htmlFor="tags">Tags: </label>
-          {/* HOW TO GROUP SIMPLER< START HERE */}
-          {/* PATTERN? */}
-          <input
-            id="tags"
-            type="text"
-            name="tags"
-            required
-          />
-        </div>
+        {inputs.tags.map((tag, index) => (
+          <div
+            className=""
+            key={index}
+          >
+            <label htmlFor="tags">Tags: </label>
+            <input
+              id="tags"
+              type="text"
+              name={`tag-${index}`}
+              required
+              pattern="^[a-zA-Z&\-\(\)', ]*$"
+              onChange={(e) => handleInputChange(index, e)}
+            />
+          </div>
+        ))}
+        <button
+          type="button"
+          onClick={handleAddTag}
+        >
+          Add Tag
+        </button>
+        <button
+          type="button"
+          onClick={handleRemoveTag}
+        >
+          Remove Tag
+        </button>
+
         <button type="submit">Submit</button>
       </form>
     </div>
