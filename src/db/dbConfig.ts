80218453c1eedd29f6652fa4c165481877d45a22
= <=>
2024-06-10 11:01:04 -0500

    set up some .env vars

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -2,16 +2,17 @@
 import pgPromise from "pg-promise";
 // const pgp = require('pg-promise')()
 // import pgp from "pg-promise";
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
 //prepare connection details
-const connectionString =
-  "postgres://postgres:password@localhost:5432/recipe_app";
+const connectionString = DB_CONNECTION_STRING;
 
 const initOptions = {};
 
 const pgp = pgPromise({});
 
 //create a new db instance
+if (!connectionString) throw new Error("DBConnectionString is null");
 const db = pgp(connectionString);
 
 export default db;
