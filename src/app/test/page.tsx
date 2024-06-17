a88bf3355a2b13b438f7c2f14164d1e991cf328a
= <=>
2024-06-17 13:44:51 -0500

    finalized recipe card update, has 3 views, makes ListRecipeCard obsolete

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,15 @@ import { testRecipe1 } from "@/lib/utils/interfaces";
 export default function Test() {
   return (
     <div className="">
-      <RecipeCard recipe={testRecipe1} />
+      <div style={{ width: "400px", height: "650px", padding: "3rem" }}>
+        <RecipeCard recipe={testRecipe1} />
+      </div>
+      <div style={{ width: "980px", height: "600px", padding: "3rem" }}>
+        <RecipeCard recipe={testRecipe1} />
+      </div>
+      <div style={{ width: "400px", height: "550px", padding: "3rem" }}>
+        <RecipeCard recipe={testRecipe1} />
+      </div>
     </div>
   );
 }
--- src/components/composite/RecipeCard/RecipeCard.tsx
+++ src/components/composite/RecipeCard/RecipeCard.tsx
@@ -6,6 +6,11 @@ import { ReactElement } from "react";
 import { createIconText, createTags } from "@/lib/utils/uiFunctions";
 import { cookies } from "next/headers";
 
+// HEIGHT <550PX WILL HIDE DESCRIPTION
+//WIDTH > 900PX WILL SHIFT TO HORIZONTAL LAYOUT
+// DEADZONES: WIDTH 480 - 900
+//            HEIGHT 550-650
+
 export default function RecipeCard({ recipe }: { recipe: IRecipe }) {
   const {
     id,
@@ -24,46 +29,48 @@ export default function RecipeCard({ recipe }: { recipe: IRecipe }) {
 
   return (
     <div className={styles.container}>
-      <div className={styles.headContainer}>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            src={img}
-            alt={`A photo of ${name}`}
-            fill={true}
-          />
-          {img ? (
-            <div className={styles.imgShade}></div>
-          ) : (
-            <div className={styles.imgShade}></div>
-          )}
+      <div className={styles.innerContainer}>
+        <div className={styles.headContainer}>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              src={img}
+              alt={`A photo of ${name}`}
+              fill={true}
+            />
+            {img ? (
+              <div className={styles.imgShade}></div>
+            ) : (
+              <div className={styles.imgShade}></div>
+            )}
 
-          <div className={styles.imgInfo}>
-            <div className={styles.imgLeftInfo}></div>
-            <div className={styles.imgRightInfo}>
-              {createIconText(
-                "fullCook.svg",
-                `${fullCookTime.toString()} mins`
-              )}
-              {user_name
-                ? createIconText("user.svg", user_name)
-                : createIconText("user.svg", "err")}
+            <div className={styles.imgInfo}>
+              <div className={styles.imgLeftInfo}></div>
+              <div className={styles.imgRightInfo}>
+                {createIconText(
+                  "fullCook.svg",
+                  `${fullCookTime.toString()} mins`
+                )}
+                {user_name
+                  ? createIconText("user.svg", user_name)
+                  : createIconText("user.svg", "err")}
+              </div>
             </div>
+            <h3 className={`${styles.recipeLink}`}>
+              <a href={`/recipe/id/${id}`}>VIEW RECIPE</a>
+            </h3>
           </div>
-          <h3 className={`${styles.recipeLink}`}>
-            <a href={`/recipe/id/${id}`}>VIEW RECIPE</a>
-          </h3>
         </div>
-      </div>
-      <div className={styles.mainContainer}>
-        <h4 className={styles.title}>
-          <a href={`/recipe/id/${id}`}>{toTitleCase(name)}</a>
-        </h4>
-        <ul className={styles.tagsContainer}>
-          {tags ? createTags(name, tags) : <div></div>}
-        </ul>
-        <p className={styles.description}>{description}</p>
-        <button className={styles.button}>Add to Meal Plan</button>
+        <div className={styles.mainContainer}>
+          <h4 className={styles.title}>
+            <a href={`/recipe/id/${id}`}>{toTitleCase(name)}</a>
+          </h4>
+          <ul className={styles.tagsContainer}>
+            {tags ? createTags(name, tags) : <div></div>}
+          </ul>
+          <p className={styles.description}>{description}</p>
+          <button className={styles.button}>Add to Meal Plan</button>
+        </div>
       </div>
     </div>
     // <div className={`${styles.card}`}>
